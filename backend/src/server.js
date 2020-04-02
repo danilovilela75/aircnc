1d21b471ff79e4dfcc21fe3aeacd0525b3ee9e8f
danilovilela75 <danilovilela@teleset.com.br>
2020-04-01 22:32:43 -0300

    deploy react from nodejs

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,9 +17,7 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
-const baseDir = `${__dirname}/htdocs/`;
-
-app.get('/', (req, res) => res.sendFile('index.html', {root: baseDir}));
+const baseDir = path.resolve(__dirname,'..','html');
 
 const connectedUsers = {};
 
@@ -38,9 +36,13 @@ app.use((req, res, next) => {
 
 const port = 8000;
 
+app.use(express.static(path.join(__dirname,'..',"build")));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'..','build','index.html')));
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
-server.listen(port, () => console.log('Servidor on line!'));
+server.listen(port, () => console.log(`Servidor iniciado http://srvteleset.ddns.net:${port}`));
